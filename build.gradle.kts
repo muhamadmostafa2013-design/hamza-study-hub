17124a142dcfcc04b29c83fb745ee97d3161c6d9
muhamadmostafa2013-design <muhamadmostafa2013@gmail.com>
2026-09-03 01:56:03 +0300

    Build: migrate AGP 9 to built-in Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,11 @@
+buildscript {
+    dependencies {
+        // AGP 9 provides Kotlin support directly. Keep KGP current without
+        // applying the legacy org.jetbrains.kotlin.android plugin.
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
+    }
+}
+
 plugins {
     id("com.android.application") version "9.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
 }
