caac70a4f2d22fb655c5a7b7bb718d044e75d0d3
muhamadmostafa2013-design <muhamadmostafa2013@gmail.com>
2026-09-02 22:19:11 +0300

    M1: add OCR for shared homework screenshots

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,4 +20,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.mlkit:text-recognition:16.0.1")
 }
