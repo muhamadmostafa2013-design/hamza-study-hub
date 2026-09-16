b2909cafcadcc6754214092ff6e9c6f97c6abd39
muhamadmostafa2013-design <muhamadmostafa2013@gmail.com>
2026-09-16 05:35:43 +0300

    M6: pin supervisor Robolectric tests to supported SDK

--- app/src/test/java/com/hamza/studyhub/agents/StudySupervisorAgentTest.kt
+++ app/src/test/java/com/hamza/studyhub/agents/StudySupervisorAgentTest.kt
@@ -7,9 +7,11 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 import java.time.LocalDate
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class StudySupervisorAgentTest {
     private val engine = StudySupervisorEngine { LocalDate.of(2026, 9, 16) }
 
